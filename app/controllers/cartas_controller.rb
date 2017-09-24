35779c0056e8d6b9bc98fd271dfc407ed9db576c
Mónica Ojeda <monica2519@gmail.com>
2017-09-24 17:34:29 -0300

    creación de controller details

--- app/controllers/cartas_controller.rb
+++ app/controllers/cartas_controller.rb
@@ -1,6 +1,10 @@
 class CartasController < ApplicationController
 	require 'mtg_sdk'
   def index
-  	@card = MTG::Card.where(name: 'berserk').all  
+  	@cards = MTG::Card.where(page: 2).where(pageSize: 100).all
+  end
+
+  def details
+  	@card = MTG::Card.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'cartas/index'
-
+resources :cartas, only: :index
+get 'cartas/:id/details', to: 'cartas#details', as: 'card_details'
+root to: 'cartas#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
